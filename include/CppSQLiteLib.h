d02369ab797c4e577b1e24a198de9577866552fa
Chatchai Daecha <chkob81@hotmail.com>
2018-03-12 22:15:49 +0700

    folder restructure to be wrapped under dll for window platform

---
+++ include/CppSQLiteLib.h
@@ -0,0 +1,21 @@
+#if defined(_WIN32)
+
+#  if (defined(_USRDLL) || defined(_WINDLL) || defined(BUILD_SHARED_LIB)) && !defined(TIDY_EXPORT) && !defined(TIDY_STATIC)
+#    ifdef CPPSQLITELIB_EXPORTS
+#      define CPPSQLITELIB_EXPORT __declspec( dllexport )
+#    else
+#      define CPPSQLITELIB_EXPORT __declspec( dllimport )
+#    endif
+#  else
+#    define CPPSQLITELIB_EXPORT extern
+#  endif
+
+#  ifndef SQLITE3_CALL
+#    ifdef _WIN64
+#      define SQLITE3_CALL __fastcall
+#    else
+#      define SQLITE3_CALL __stdcall
+#    endif
+#  endif
+
+#endif /* _WIN32 */
